f296f061725813b2080f43b088f25e79981c5eef
Azie Amiza <22FTT1497@student.pb.edu.bn>
2024-03-18 23:32:52 +0800

    Add the prefix "as2-" to the compressed zip file name

--- config/backup.php
+++ config/backup.php
@@ -140,7 +140,7 @@ return [
             /*
              * The filename prefix used for the backup zip file.
              */
-            'filename_prefix' => '',
+            'filename_prefix' => 'as2-',
 
             /*
              * The disk names on which the backups will be stored.
